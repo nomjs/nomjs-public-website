a7a6636719e4768faf6a59aef2ef66f545d48f0f
Michael Laccetti <michael@laccetti.com>
2016-04-04 12:31:30 -0400

    More deploy fiddling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Current directory: "$(pwd)""
-cd ../public && find . -type f -exec curl -u ${AZURE_WA_SITE}\\${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD} --ftp-create-dirs -T {} ${AZURE_WA_FTP_SITE}/{} \;
+ls -alsrth
+cd public && find . -type f -exec curl -u ${AZURE_WA_SITE}\\${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD} --ftp-create-dirs -T {} ${AZURE_WA_FTP_SITE}/{} \;
