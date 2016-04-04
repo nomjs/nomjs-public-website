3cb262e1be8b8fe2eded37242b37337130d1b511
Michael Laccetti <michael@laccetti.com>
2016-04-04 12:25:49 -0400

    Find me.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo "Current directory: "$(pwd)""
 cd ../public && find . -type f -exec curl -u ${AZURE_WA_SITE}\\${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD} --ftp-create-dirs -T {} ${AZURE_WA_FTP_SITE}/{} \;
